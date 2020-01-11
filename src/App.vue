599ae97fd08723c2924023091ecb1a2cfa5490a8
Nord18 <andrey.zhilyaev@gmail.com>
2020-01-11 06:00:40 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'app',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+@import '~bootstrap/dist/css/bootstrap.min.css';
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="container pt-3">
+    <div class="row justify-content-around mb-3">
+      <div class="col-md-4">
+        <button @click="showField = !showField" type="button" class="btn btn-success mr-1">Создать новый лист</button>
+        <button @click="clearTodoList" type="button" class="btn btn-danger">Удалить все</button>
+      </div>
+      <div class="input-group col-md-4">
+        <input v-model="searchTodo" type="text" class="form-control" placeholder="Search for..." aria-label="Recipient's username" aria-describedby="button-addon2">
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="button" id="button-addon2">Go!</button>
+        </div>
+      </div>
+    </div>
+    <div v-show="showField" class="row justify-content-center mb-2">
+      <form class="col-md-4" @submit.prevent="createTodo">
+        <div class="input-group">
+          <input v-model="todoTitle" type="text" class="form-control" placeholder="описание..." aria-label="Recipient's username" aria-describedby="button-addon2">
+          <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Ок!</button>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div v-for="item in getTodoList" :key="item.id">
+      <div class="shadow-lg p-3 mb-2 bg-white rounded">
+        <div class="row align-items-center pl-1">
+          <div v-if="item.isChange" class="mr-1">
+            <input :value="item.name" @change="setNewTodoName($event)" type="text" class="form-control">
+          </div>
+          <div v-else class="mr-1">{{ item.name }}</div>
+          <div v-if="item.isChange">
+            <button @click="saveNameTodo(item)" type="button" class="btn btn-success mr-1">Сохранить</button>
+            <button @click="cancelChange(item)" type="button" class="btn btn-danger">Отмена</button>
+          </div>
+          <div v-else>
+            <button @click="changeNameTodo(item)" type="button" class="btn btn-warning mr-1 text-white">Редактировать</button>
+            <button @click="deleteTodo(item)" type="button" class="btn btn-danger">Удалить</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data() {
+    return {
+      showField: false,
+      todoTitle: '',
+      searchTodo: '',
+      todoList: [],
+      newNameTodo: ''
+    }
+  },
+  computed: {
+    getTodoList () {
+      return this.todoList.filter(item => {
+        return item.name.toLowerCase().includes(this.searchTodo.toLowerCase())
+      })
+    }
+  },
+  methods: {
+    createTodo () {
+      if (this.todoTitle !== '') {
+        this.todoList.push({
+          name: this.todoTitle,
+          id: Math.random(),
+          isChange: false
+        })
+        this.showField = false
+        this.todoTitle = ''
+      } else {
+        alert('Введите название')
+      }
+    },
+    deleteTodo (item) {
+      this.todoList.splice(this.todoList.indexOf(item), 1)
+    },
+    clearTodoList () {
+      this.todoList = []
+    },
+    changeNameTodo (item) {
+      item.isChange = true
+      this.newNameTodo = ''
+    },
+    cancelChange (item) {
+      item.isChange = false
+      this.newNameTodo = ''
+    },
+    saveNameTodo (item) {
+      if (this.newNameTodo !== '') {
+        item.name = this.newNameTodo
+        item.isChange = false
+      } else {
+        alert('Поле не может быть пустым')
+      }
+    },
+    setNewTodoName (evt) {
+      this.newNameTodo = evt.target.value
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
